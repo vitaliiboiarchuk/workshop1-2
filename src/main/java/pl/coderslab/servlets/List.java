8be2fbcd5c4d697d87931a06374167e192df9343
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2022-10-02 11:24:42 +0200

    list

--- src/main/java/pl/coderslab/servlets/List.java
+++ src/main/java/pl/coderslab/servlets/List.java
@@ -1,5 +1,8 @@
 package pl.coderslab.servlets;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,6 +12,12 @@ import java.io.IOException;
 public class List extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserDAO userDAO = new UserDAO();
+        User[] users = userDAO.findAll();
+
+        request.setAttribute("users",users);
+
         getServletContext().getRequestDispatcher("/list.jsp").forward(request,response);
     }
 
